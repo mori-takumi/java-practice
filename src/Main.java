1cfdb7a7e4e6764d294d82a972f6e9bd574fd070
森拓実 <moritakumi@moritakuminoMacBook-Pro.local>
2022-12-01 12:02:35 +0900

    datetaimeformatterの実装

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) {
         List<User> users = List.of(
                 new User("佐藤美咲", LocalDate.of(1990, 1, 1)),
@@ -11,6 +13,15 @@ public class Main {
 
         System.out.println("【すべてのユーザーを表示する】");
         users.forEach(u -> System.out.printf("名前: %s, 生年月日: %s\n", u.getName(), u.getBirthdate()));
+
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        new User("佐藤美咲", DateTimeFormatter.ofPattern("1990, 1, 1"));
+        new User("鈴木太郎", DateTimeFormatter.ofPattern("1991, 2, 2"));
+        new User("山田一郎", DateTimeFormatter.ofPattern("2003, 3, 3"));
+        new User("鈴木花子", DateTimeFormatter.ofPattern("2002, 4, 4"));
+
+        System.out.println("【すべてのユーザーを表示する】");
+        users.forEach(u -> System.out.printf("名前: %s, 生年月日: %s\n", u.getName(), u.getBirthdate()));
     }
 }
-
